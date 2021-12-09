3c8cfcf25ddf42cf61ad6745ed963f59345cc45b
AneriPatel45 <92508439+AneriPatel45@users.noreply.github.com>
2021-12-09 12:33:07 +0530

    css path

--- common_files/css_path.py
+++ common_files/css_path.py
@@ -1,5 +1,3 @@
-
-
 city_search= 'css:#hotels-search > div > div > div.col-md-4 > div > div > div > span > span.selection > span'
 calender_checkin=  'css:#checkin' 
 calender_checkout=  'css:#checkout '
@@ -11,7 +9,8 @@ search=  'css:#submit'
 travellers= 'css:#hotels-search > div > div > div:nth-child(3) > div > div > div > a'
 inc_rooms= ' css:#hotels-search > div > div > div:nth-child(3) > div > div > div > div > div:nth-child(1) > div > div > div.roomInc'
 inc_adults = 'css:#hotels-search > div > div > div:nth-child(3) > div > div > div > div > div:nth-child(2) > div > div > div.qtyInc'
-
-
 input_name=  'css:body > span > span > span.select2-search.select2-search--dropdown > input'
 name_click=  'css:#select2-hotels_city-results > li:nth-child(1)'
+inc_childs= 'css:#hotels-search > div > div > div:nth-child(3) > div > div > div > div > div:nth-child(3) > div > div > div.qtyInc'
+childs_age_field = 'css:#ages1'
+childs_age = 'css:#ages1 > option:nth-child(8)'
